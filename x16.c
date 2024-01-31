035e58ccc20d03ed3dc58c25753627d22ead428b
Laviniam17 <158043270+Laviniam17@users.noreply.github.com>
2024-01-31 16:03:26 +0000

    lezione 4

--- x16.c
+++ x16.c
@@ -4,25 +4,36 @@ void calcolatrice()
 {
     int x;
     int y;
-    int operazione= 0;
+    char operazione;
 
-    printf("inserisci numero");
-    scanf("%d", &x);
-    printf("inserisci un secondo numero");
-    scanf("%d", &y);
+    printf("inserisci numero\n");
+    scanf(" %d", &x);
 
-    printf("quale operazione vuoi eseguire su x e y");
-    printf("inserisci 1 per +");
-    printf("inserisci 2 per -");
-    printf("inserisci 3 per *");
-    printf("inserisci 4 per /");
+    printf("inserisci un secondo numero\n");
+    scanf(" %d", &y);
 
-    scanf( "%d", &operazione);
+    printf("quale operazione vuoi eseguire su x e y?\n");
+    scanf( " %c", &operazione);
 
-    if(operazione == 1 || operazione == 2 || operazione  == 3 || operazione == 4)
-     {
-        scanf("%d", &operazione);
-    }
+   
+     
+        if(operazione == '+' ) 
+        {
+            printf("somma= %d", x+y);
+        }
+        else if(operazione == '-' )
+        {
+        printf("sottrazione= %d", x-y);
+        }
+        else if(operazione == '*' )
+        {
+         printf("moltiplicazione= %d", x*y);
+        }
+        else if(operazione == '/' )
+        {
+        printf("divisione= %d", x/y);
+        }
+    
 
 }
 int main()
